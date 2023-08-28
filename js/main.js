3e83517dc5b7b0800c67473a3bceb56022841f21
dayiSync <137202093+JuanDavidSerranoValencia@users.noreply.github.com>
2023-08-28 13:27:58 -0500

    busqueda

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+import {searchAll} from "./get.js";
+document.querySelector("#charSearch").addEventListener("change",(e)=>{
+    searchAll(e.target.value)
+})
+
+
+
+
+
+
+
+
+
+
+
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
 var container = document.querySelector(".container");
@@ -6,3 +21,6 @@ menuIcon.onclick=function(){
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container")
 }
+
+
+
