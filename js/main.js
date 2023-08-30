e08946885afc19ec81895bd7b076706da283b4a5
JuanDavidSerranoValencia <serranovalenciajuandavid949@gmail.com>
2023-08-30 07:26:02 -0500

    feat: :sparkles: Consumo Json

--- js/main.js
+++ js/main.js
@@ -3,36 +3,51 @@ document.querySelector("#charSearch").addEventListener("change",(e)=>{
     searchAll(e.target.value)
 })*/
 
-let fondoCanal = async ()=>{
-    let peticion = await fetch(`/storage/canal.json`);
+
+let fotoCanal = async ()=>{
+    let peticion = await fetch(`storage/canal.json`);
     let res = await peticion.json();
-    let seleccion = document.querySelector(".container");
-    seleccion.insertAdjacentHTML  ("beforeend"/*html*/`
-    ${res.banner.map((values)=>/*html*/`
-    <div class="banner">
-        <img src=${values.desktop.url} alt="">
-    </div>`)}
+    let seleccion = document.querySelector(".nav-right");
+    seleccion.insertAdjacentHTML  ("afterbegin",/*html*/`
    
 
-    
+        <img src="images/upload.png">
+        <img src="images/more.png">
+        <img src="images/notification.png">
+        <img src=${res.avatar[1].url} class="user-icon">
+
         `)};
+fotoCanal()
+
+let fondoCanal = async ()=>{
+    let peticion = await fetch(`storage/canal.json`);
+    let res = await peticion.json();
+    let seleccion = document.querySelector(".container");
+    seleccion.insertAdjacentHTML  ("afterbegin",/*html*/`
+    <div class="banner">
+        <img src=${res.banner.desktop[0].url} alt="">
+    </div>
+        `)};
+fondoCanal()
 
 
 let fondoVideo = async ()=>{
-    let peticion = await fetch(`/storage/channelSearch.json`);
+    let peticion = await fetch(`storage/channelSearch.json`);
+    let foto= await fetch(`storage/canal.json`);
+    let fotoC = await foto.json();
     let res = await peticion.json();
     let seleccion = document.querySelector(".list-container");
     seleccion.insertAdjacentHTML  ("beforeend",/*html*/`
     ${res.videos.map((value)=>/*html*/`
     <div class="vid-list">
-    <a href="videos.html"><img src=${value.thumbnails.url}
+    <a href="videos.html"><img src=${value.thumbnails[3].url}
         class="thumbnail"></a> 
         <div class="flex-div">
-            <img src="images/Jack.png">
+            <img src=${fotoC.avatar[1].url}>
             <div class="vid-info">
-                <a href="videos.html">${res.title}</a>
-                <p>Easy Tutorials</p>
-                <p>15k Views &bull; 2 days</p>
+                <a href="videos.html">${value.title}</a>
+                <p>CreativeCode</p>
+                <p>${value.number_of_views}&bull; ${value.published_time}</p>
             </div>
         </div>
     </div>` ).join("")}
