bca43035b8108e65833b3c7ae79a7db2f65acfbd
JuanDavidSerranoValencia <serranovalenciajuandavid949@gmail.com>
2023-08-29 21:14:53 -0500

    feat: :art:pagina Principal

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
-import {searchAll} from "./get.js";
+/*import {searchAll} from "./get.js";
 document.querySelector("#charSearch").addEventListener("change",(e)=>{
     searchAll(e.target.value)
-})
+})*/
+
+let fondoCanal = async ()=>{
+    let peticion = await fetch(`/storage/canal.json`);
+    let res = await peticion.json();
+    let seleccion = document.querySelector(".container");
+    seleccion.insertAdjacentHTML  ("beforeend"/*html*/`
+    ${res.banner.map((values)=>/*html*/`
+    <div class="banner">
+        <img src=${values.desktop[0].url} alt="">
+    </div>`)}
+   
+    
+        `)};
 
 
-
-
-
-
-
-
-
+let fondoVideo = async ()=>{
+    let peticion = await fetch(`/storage/channelSearch.json`);
+    let res = await peticion.json();
+    let seleccion = document.querySelector(".list-container");
+    seleccion.insertAdjacentHTML  ("beforeend"/*html*/`
+    ${res.videos.map((value)=>/*html*/`
+    <div class="vid-list">
+        <a href="videos.html"><img src=${value.thumbnails.url} class="thumbnail"></a> 
+        <div class="flex-div">
+            <img src="images/Jack.png">
+            <div class="vid-info">
+                <a href="videos.html">Best channel to learn coding that help you to be a with 
+                developer</a>
+                <p>Easy Tutorials</p>
+                <p>15k Views &bull; 2 days</p>
+            </div>
+        </div>
+    </div>` ).join("")}
+        `)};
+       
 
 
 var menuIcon = document.querySelector(".menu-icon");
@@ -23,4 +49,4 @@ menuIcon.onclick=function(){
 }
 
 
-
+fondoVideo();
